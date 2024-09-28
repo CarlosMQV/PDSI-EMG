d8dbc63d8d6f5b6688eb5e438b6c49eb008f1e3c
Carlos <carlosmarceloqv@gmail.com>
2024-09-28 00:31:57 -0500

    Reduced code.

--- data_visualizer.py
+++ data_visualizer.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#Dataframes del sujeto 1
+# Dataframes del sujeto 1
 df1 = pd.read_parquet('datasets/ninapro/DB6_s1_a/S1_D1_T1.parquet', engine='pyarrow')
 df2 = pd.read_parquet('datasets/ninapro/DB6_s1_a/S1_D1_T2.parquet', engine='pyarrow')
 df3 = pd.read_parquet('datasets/ninapro/DB6_s1_a/S1_D2_T1.parquet', engine='pyarrow')
@@ -9,7 +9,7 @@ df4 = pd.read_parquet('datasets/ninapro/DB6_s1_a/S1_D2_T2.parquet', engine='pyar
 df5 = pd.read_parquet('datasets/ninapro/DB6_s1_a/S1_D3_T1.parquet', engine='pyarrow')
 df6 = pd.read_parquet('datasets/ninapro/DB6_s1_a/S1_D3_T2.parquet', engine='pyarrow')
 df_s1 = pd.concat([df1, df2, df3, df4, df5, df6], ignore_index=True)
-#Dataframes del sujeto 2
+# Dataframes del sujeto 2
 df1 = pd.read_parquet('datasets/ninapro/DB6_s2_a/S2_D1_T1.parquet', engine='pyarrow')
 df2 = pd.read_parquet('datasets/ninapro/DB6_s2_a/S2_D1_T2.parquet', engine='pyarrow')
 df3 = pd.read_parquet('datasets/ninapro/DB6_s2_a/S2_D2_T1.parquet', engine='pyarrow')
@@ -17,20 +17,9 @@ df4 = pd.read_parquet('datasets/ninapro/DB6_s2_a/S2_D2_T2.parquet', engine='pyar
 df5 = pd.read_parquet('datasets/ninapro/DB6_s2_a/S2_D3_T1.parquet', engine='pyarrow')
 df6 = pd.read_parquet('datasets/ninapro/DB6_s2_a/S2_D3_T2.parquet', engine='pyarrow')
 df_s2 = pd.concat([df1, df2, df3, df4, df5, df6], ignore_index=True)
-#Eliminamos variables inútiles. Si se desea conservarlas, comentar.
-del df1
-del df2
-del df3
-del df4
-del df5
-del df6
 
-# Eliminamos las columnas 9 y 10
-df_s1 = df_s1.drop(columns=[df_s1.columns[8], df_s1.columns[9]])
-df_s2 = df_s2.drop(columns=[df_s2.columns[8], df_s2.columns[9]])
-#Creamos un solo dataframe
+#Creamos un solo dataframe y eliminamos columnas 9 y 10
 df = pd.concat([df_s1, df_s2], ignore_index=True)
-#Eliminamos variables inútiles. Si se desea conservarlas, comentar.
-del df_s1
-del df_s2
+df.drop(columns=[df.columns[8], df.columns[9]])
 
+conteo_por_estado = df[df.columns[-1]].value_counts()
