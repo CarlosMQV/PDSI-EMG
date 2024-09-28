a8b2518b2918056fc70cf1f0d32e5a71318f7936
Carlos <carlosmarceloqv@gmail.com>
2024-09-28 01:09:15 -0500

    First data visualization. Here we have the EMG signal of the first sensor for the first grasp. There are 12 repetitions.

--- data_visualizer.py
+++ data_visualizer.py
@@ -20,6 +20,23 @@ df_s2 = pd.concat([df1, df2, df3, df4, df5, df6], ignore_index=True)
 
 #Creamos un solo dataframe y eliminamos columnas 9 y 10
 df = pd.concat([df_s1, df_s2], ignore_index=True)
-df.drop(columns=[df.columns[8], df.columns[9]])
+df = df.drop(columns=[df.columns[8], df.columns[9]])
 
 conteo_por_estado = df[df.columns[-1]].value_counts()
+
+c_estimulo = df.columns[-1]
+df['bloque'] = (df[c_estimulo] != df[c_estimulo].shift()).cumsum()
+
+df_filtrado = df[df['bloque'].between(1, 24)]
+
+# Seleccionar los valores de la primera columna (primer sensor)
+valores_sensor_1 = df_filtrado.iloc[:, 0]  # La primera columna del DataFrame
+
+# Graficar los valores del primer sensor
+plt.figure(figsize=(12, 6))
+plt.plot(valores_sensor_1.index, valores_sensor_1, label='Sensor 1')
+plt.title('Señal EMG del primer sensor (Bloques 1 al 24)')
+plt.xlabel('Muestras')
+plt.ylabel('Amplitud de señal EMG')
+plt.legend(loc='upper right')
+plt.show()
