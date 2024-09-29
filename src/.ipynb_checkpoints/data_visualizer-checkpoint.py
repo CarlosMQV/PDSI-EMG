2d2d1bb16c4329c2e3ca8043f7f98dc38c6d5f5a
Carlos <carlosmarceloqv@gmail.com>
2024-09-29 00:55:52 -0500

    A fokin change with a ipynb (again)

--- src/.ipynb_checkpoints/data_visualizer-checkpoint.py
+++ src/.ipynb_checkpoints/data_visualizer-checkpoint.py
@@ -4,20 +4,20 @@ import matplotlib.pyplot as plt
 #---------------------------------------------------------------------------------
 def lectura():
     # Dataframes del sujeto 1
-    df1 = pd.read_parquet('datasets/ninapro/DB6_s1_a/S1_D1_T1.parquet', engine='pyarrow')
-    df2 = pd.read_parquet('datasets/ninapro/DB6_s1_a/S1_D1_T2.parquet', engine='pyarrow')
-    df3 = pd.read_parquet('datasets/ninapro/DB6_s1_a/S1_D2_T1.parquet', engine='pyarrow')
-    df4 = pd.read_parquet('datasets/ninapro/DB6_s1_a/S1_D2_T2.parquet', engine='pyarrow')
-    df5 = pd.read_parquet('datasets/ninapro/DB6_s1_a/S1_D3_T1.parquet', engine='pyarrow')
-    df6 = pd.read_parquet('datasets/ninapro/DB6_s1_a/S1_D3_T2.parquet', engine='pyarrow')
+    df1 = pd.read_parquet('../datasets/ninapro/DB6_s1_a/S1_D1_T1.parquet', engine='pyarrow')
+    df2 = pd.read_parquet('../datasets/ninapro/DB6_s1_a/S1_D1_T2.parquet', engine='pyarrow')
+    df3 = pd.read_parquet('../datasets/ninapro/DB6_s1_a/S1_D2_T1.parquet', engine='pyarrow')
+    df4 = pd.read_parquet('../datasets/ninapro/DB6_s1_a/S1_D2_T2.parquet', engine='pyarrow')
+    df5 = pd.read_parquet('../datasets/ninapro/DB6_s1_a/S1_D3_T1.parquet', engine='pyarrow')
+    df6 = pd.read_parquet('../datasets/ninapro/DB6_s1_a/S1_D3_T2.parquet', engine='pyarrow')
     df_s1 = pd.concat([df1, df2, df3, df4, df5, df6], ignore_index=True)
     # Dataframes del sujeto 2
-    df1 = pd.read_parquet('datasets/ninapro/DB6_s2_a/S2_D1_T1.parquet', engine='pyarrow')
-    df2 = pd.read_parquet('datasets/ninapro/DB6_s2_a/S2_D1_T2.parquet', engine='pyarrow')
-    df3 = pd.read_parquet('datasets/ninapro/DB6_s2_a/S2_D2_T1.parquet', engine='pyarrow')
-    df4 = pd.read_parquet('datasets/ninapro/DB6_s2_a/S2_D2_T2.parquet', engine='pyarrow')
-    df5 = pd.read_parquet('datasets/ninapro/DB6_s2_a/S2_D3_T1.parquet', engine='pyarrow')
-    df6 = pd.read_parquet('datasets/ninapro/DB6_s2_a/S2_D3_T2.parquet', engine='pyarrow')
+    df1 = pd.read_parquet('../datasets/ninapro/DB6_s2_a/S2_D1_T1.parquet', engine='pyarrow')
+    df2 = pd.read_parquet('../datasets/ninapro/DB6_s2_a/S2_D1_T2.parquet', engine='pyarrow')
+    df3 = pd.read_parquet('../datasets/ninapro/DB6_s2_a/S2_D2_T1.parquet', engine='pyarrow')
+    df4 = pd.read_parquet('../datasets/ninapro/DB6_s2_a/S2_D2_T2.parquet', engine='pyarrow')
+    df5 = pd.read_parquet('../datasets/ninapro/DB6_s2_a/S2_D3_T1.parquet', engine='pyarrow')
+    df6 = pd.read_parquet('../datasets/ninapro/DB6_s2_a/S2_D3_T2.parquet', engine='pyarrow')
     df_s2 = pd.concat([df1, df2, df3, df4, df5, df6], ignore_index=True)
     
     # Creamos un solo dataframe y eliminamos columnas 9 y 10
